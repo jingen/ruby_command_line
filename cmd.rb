cca53e17101770a7dbc4d4792dd791c819d2b2c3
Jingen Lin <jingen.lin.jl@gmail.com>
2013-08-30 14:29:51 -0400

    :2013-08-30 14:29:51 -0400

--- cmd.rb
+++ cmd.rb
@@ -3,12 +3,29 @@
 require "thor"
 
 class Commands < Thor
-	desc "commit x", "git commit -am message"
+	desc "commit message", "c c message"
 	def c message=""
 		executed_cmd = get_commit(message)
 		puts %x(#{executed_cmd})
 	end
 
+	desc "initalize repository", "c i origin_repository"
+	option :exist, aliases: "-e"
+	def i repo
+		if options[:exist].nil?
+			executed_cmd = get_init(repo)
+		else
+			executed_cmd = get_init(repo, "exist")
+		end
+		puts %x(#{executed_cmd})
+	end
+
+	desc "clone repository", "c clone repository"
+	def clone repo
+		executed_cmd = "git clone #{repo}"
+		puts %x(#{executed_cmd})
+	end
+	
 	private
 
 	def get_commit message = "fix"
@@ -17,6 +34,19 @@ class Commands < Thor
 		cmd << "git commit -am " + "'" + message + " :" + Time.now.to_s + "'"
 		cmd << "git pull"
 		cmd << "git push"
+		return cmd.join(" && ")
+	end
+
+	def get_init (repo,  exist=nil)
+		cmd = Array.new
+		if exist.nil?
+			cmd << "touch README.md"
+			cmd << "git init"
+			cmd << "git add README.md"
+			cmd << "git commit -m 'first commit'"
+		end
+		cmd << "git remote add origin #{repo}"
+		cmd << "git push -u origin master"
 		return cmd.join(" && ")
 	end
 end
